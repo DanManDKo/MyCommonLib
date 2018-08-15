a9b5c757ba8560f743cac8c8061c773df9ee5276
shcherbininin <sasha.shcherbinin@gmail.com>
2018-08-15 11:45:51 +0300

    Memory list improvements

--- library/src/main/java/com/sprinklebit/library/utils/SizeUtil.kt
+++ library/src/main/java/com/sprinklebit/library/utils/SizeUtil.kt
@@ -15,7 +15,7 @@ class SizeUtil {
          * This method converts dp unit to equivalent pixels, depending on device density.
          *
          * @param dp A value in dp (density independent pixels) unit. Which we need to convert into pixels
-         * @param context Context to get resources and device specific display metrics
+         * @param context Context to observe resources and device specific display metrics
          * @return A float value to represent px equivalent to dp depending on device density
          */
         fun convertDpToPixel(dp: Float, context: Context): Float {
@@ -28,7 +28,7 @@ class SizeUtil {
          * This method converts device specific pixels to density independent pixels.
          *
          * @param px A value in px (pixels) unit. Which we need to convert into db
-         * @param context Context to get resources and device specific display metrics
+         * @param context Context to observe resources and device specific display metrics
          * @return A float value to represent dp equivalent to px value
          */
         fun convertPixelsToDp(px: Float, context: Context): Float {
