4ac74c91c224e607283dc4471e5101870ac275e7
sasha.shcherbinin <sasha.shcherbinin@gmail.com>
2019-03-03 10:01:52 +0200

    Deprecate

--- library/src/main/java/com/sprinklebit/library/utils/OneTimeEvent.kt
+++ library/src/main/java/com/sprinklebit/library/utils/OneTimeEvent.kt
@@ -4,6 +4,7 @@ package com.sprinklebit.library.utils
  * User: Sasha Shcherbinin
  * Date : 5/22/18
  */
+@Deprecated("Check on saveInstance == null and call method with parameters.")
 class OneTimeEvent constructor(private val event: () -> Unit) {
 
     private var firstTime: Boolean = true
