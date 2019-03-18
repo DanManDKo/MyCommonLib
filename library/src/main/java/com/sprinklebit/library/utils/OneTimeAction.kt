702fa021340d53d0beb832f7d43c5e8000452e4f
sasha.shcherbinin <sasha.shcherbinin@gmail.com>
2019-03-18 17:46:04 +0200

    Dipricated method

--- library/src/main/java/com/sprinklebit/library/utils/OneTimeAction.kt
+++ library/src/main/java/com/sprinklebit/library/utils/OneTimeAction.kt
@@ -4,6 +4,7 @@ package com.sprinklebit.library.utils
  * User: Sasha Shcherbinin
  * Date : 5/22/18
  */
+@Deprecated("use OneTimeActionWithParameter instead")
 class OneTimeAction constructor(private val event: () -> Unit) {
 
     private var firstTime: Boolean = true
