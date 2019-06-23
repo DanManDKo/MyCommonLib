7dac0d7357dea4a361fe84713b7767b55b1f7bb9
sasha.shcherbinin <sasha.shcherbinin@gmail.com>
2019-06-23 18:54:06 +0300

    Fix with graph view

--- library/src/main/java/com/sprinklebit/library/presentation/widget/graph/GraphView.kt
+++ library/src/main/java/com/sprinklebit/library/presentation/widget/graph/GraphView.kt
@@ -11,12 +11,10 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.highlight.Highlight
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.sprinklebit.library.R
 import com.sprinklebit.library.domain.model.ChartPoint
 import com.sprinklebit.library.utils.SizeUtils
-import java.util.*
 
 class GraphView(context: Context, attrs: AttributeSet? = null)
     : TouchedLineChart(context, attrs) {
@@ -52,7 +50,7 @@ class GraphView(context: Context, attrs: AttributeSet? = null)
 
         initMarker()
         setBackgroundColor(backgroundResourceId)
-        updateChart()
+        setupChart()
     }
 
     private fun initMarker() {
@@ -61,7 +59,7 @@ class GraphView(context: Context, attrs: AttributeSet? = null)
         this.marker = markerView
     }
 
-    private fun updateChart() {
+    private fun setupChart() {
         val xAxisRenderer = NoOverlappingLabelsXAxisRenderer(this)
         xAxisRenderer.setLabelSpacing(SizeUtils.convertDpToPixel(5f, context).toInt())
         this.setXAxisRenderer(xAxisRenderer)
@@ -81,6 +79,8 @@ class GraphView(context: Context, attrs: AttributeSet? = null)
         intiLeftYAxis()
         intiRightYAxis()
         this.xAxis.isEnabled = false
+        this.xAxis.setDrawLabels(isXValuesEnabled)
+        setViewPortOffsets(0f, markerView.height.toFloat(), 0f, 0f)
     }
 
     private fun intiLeftYAxis() {
@@ -109,30 +109,20 @@ class GraphView(context: Context, attrs: AttributeSet? = null)
     fun setPoints(points: List<ChartPoint>) {
         if (this.points == points) return
         this.points = points
-        if (points.isEmpty()) {
+        updatePoints()
+    }
+
+    private fun updatePoints() {
+        if (points == null) return
+        if (points!!.isEmpty()) {
             this.clear()
             return
         }
 
-        val lowestVisibleX = this.lowestVisibleX
-        this.data = createLineData(points)
-        val xAxis = this.xAxis
-        xAxis.setDrawLabels(isXValuesEnabled)
+        this.data = createLineData(points!!)
+        this.setVisibleXRangeMinimum(points!!.size.toFloat())
 
-        setViewPortOffset()
-        updateChart()
-
-        val minValues = points.size.toFloat()
-        val minXVisibleRange = getMinVisibleXRange()
-        this.setVisibleXRangeMinimum(if (minXVisibleRange >= 0) minXVisibleRange else minValues)
-
-        if (!doesPreviousDataExist()) {
-            var lowestIndex = points.size.toFloat() - 1f - minValues
-            lowestIndex = if (lowestIndex < 0) 0f else lowestIndex
-            this.moveViewToX(lowestIndex)
-        } else if (lowestVisibleX > 0) {
-            this.moveViewToX(lowestVisibleX)
-        }
+        invalidate()
     }
 
     private fun createLineData(points: List<ChartPoint>): LineData {
@@ -146,20 +136,8 @@ class GraphView(context: Context, attrs: AttributeSet? = null)
         set.setDrawCircleHole(false)
         set.setDrawCircles(false)
         set.setDrawValues(false)
-        val drawable = getFillDrawable()
-        set.fillDrawable = drawable
-        val dataSets = ArrayList<ILineDataSet>()
-        dataSets.add(set)
-        return LineData(dataSets)
-    }
-
-    private fun setViewPortOffset() {
-        if (isXValuesEnabled) {
-            val bottomOffset = SizeUtils.convertDpToPixel(20f, context)
-            this.setViewPortOffsets(0f, markerView.height.toFloat(), 0f, bottomOffset)
-        } else {
-            this.setViewPortOffsets(0f, markerView.height.toFloat(), 0f, 0f)
-        }
+        set.fillDrawable = getFillDrawable()
+        return LineData(set)
     }
 
     private fun doesPreviousDataExist(): Boolean {
@@ -186,28 +164,24 @@ class GraphView(context: Context, attrs: AttributeSet? = null)
         this.highlightValue(null)
     }
 
-    fun setMarkerValueFormatter(valueFormatter: ((value: Float) -> String)?){
+    fun setMarkerValueFormatter(valueFormatter: ((value: Float) -> String)?) {
         markerView.valueFormatter = valueFormatter
     }
 
     fun setLineColor(lineColor: Int) {
         this.lineColor = lineColor
-        updateChart()
-    }
-
-    private fun getMinVisibleXRange(): Float {
-        return -1f
+        updatePoints()
     }
 
     private fun getFillDrawable(): Drawable? = fillDrawable
 
     fun setFillDrawable(drawable: Drawable?) {
         this.fillDrawable = drawable
-        updateChart()
+        updatePoints()
     }
 
     fun setSelectedListener(listener: ((point: ChartPoint?) -> Unit)?) {
-        this.valueSelectedListener =  listener
+        this.valueSelectedListener = listener
         this.setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
             override fun onNothingSelected() {
                 valueSelectedListener?.invoke(null)
