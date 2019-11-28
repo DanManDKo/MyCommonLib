c8f7a846d76433c8aea2fafe6f162272a5fa83bb
sasha.shcherbinin <sasha.shcherbinin@gmail.com>
2019-11-28 11:54:33 +0200

    Updated theme utils

--- library/src/main/java/com/sprinklebit/library/utils/ThemeUtils.kt
+++ library/src/main/java/com/sprinklebit/library/utils/ThemeUtils.kt
@@ -1,6 +1,7 @@
 package com.sprinklebit.library.utils
 
 import android.content.Context
+import android.content.res.ColorStateList
 
 object ThemeUtils {
 
@@ -9,6 +10,15 @@ object ThemeUtils {
         val a = context.obtainStyledAttributes(intArrayOf(attr))
         try {
             return a.getColor(0, 0)
+        } finally {
+            a.recycle()
+        }
+    }
+
+    fun getColorStateList(context: Context, attr: Int): ColorStateList? {
+        val a = context.obtainStyledAttributes(intArrayOf(attr))
+        try {
+            return a.getColorStateList(0)
         } finally {
             a.recycle()
         }
