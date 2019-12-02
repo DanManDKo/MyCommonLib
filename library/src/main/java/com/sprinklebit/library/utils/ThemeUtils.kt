f9b37452ca7d091e4edd79b4ae612b7315fe3a3c
sasha.shcherbinin <sasha.shcherbinin@gmail.com>
2019-12-02 12:07:38 +0200

    Updated theme utils

--- library/src/main/java/com/sprinklebit/library/utils/ThemeUtils.kt
+++ library/src/main/java/com/sprinklebit/library/utils/ThemeUtils.kt
@@ -1,26 +1,19 @@
+@file:Suppress("unused")
+
 package com.sprinklebit.library.utils
 
 import android.content.Context
 import android.content.res.ColorStateList
+import android.content.res.Resources
 import android.graphics.drawable.Drawable
 
 object ThemeUtils {
 
     @JvmStatic
-    fun getColorFromAttrRes(context: Context, attr: Int): Int {
+    fun getColorFromAttr(context: Context, attr: Int): Int {
         val a = context.obtainStyledAttributes(intArrayOf(attr))
         try {
             return a.getColor(0, 0)
-        } finally {
-            a.recycle()
-        }
-    }
-
-    @JvmStatic
-    fun getDrawableFromRes(context: Context, attr: Int): Drawable? {
-        val a = context.obtainStyledAttributes(intArrayOf(attr))
-        try {
-            return a.getDrawable(0)
         } finally {
             a.recycle()
         }
@@ -31,6 +24,34 @@ object ThemeUtils {
         val a = context.obtainStyledAttributes(intArrayOf(attr))
         try {
             return a.getColorStateList(0)
+        } finally {
+            a.recycle()
+        }
+    }
+
+    fun getColorFromAttr(theme: Resources.Theme, attr: Int): Int {
+        val a = theme.obtainStyledAttributes(intArrayOf(attr))
+        try {
+            return a.getColor(0, 0)
+        } finally {
+            a.recycle()
+        }
+    }
+
+    fun getColorStateListFromAttr(theme: Resources.Theme, attr: Int): ColorStateList? {
+        val a = theme.obtainStyledAttributes(intArrayOf(attr))
+        try {
+            return a.getColorStateList(0)
+        } finally {
+            a.recycle()
+        }
+    }
+
+    @JvmStatic
+    fun getDrawableFromAttr(context: Context, attr: Int): Drawable? {
+        val a = context.obtainStyledAttributes(intArrayOf(attr))
+        try {
+            return a.getDrawable(0)
         } finally {
             a.recycle()
         }
